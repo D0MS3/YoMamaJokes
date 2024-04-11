a0057bbb0e1a5495231202b70f7d9f5305821297
D0MS3 <d.lehnberg@gmx.de>
2024-04-11 18:10:53 +0200

    added TKinter UI for first tests

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
 import json
 from argparse import ArgumentParser
-from src.JokeFinder import JokeFinder
+import src.JokeFinder as JF
+import tkinter as TK
 
-YO_MAMA_PATH = 'yo_mama_jokes.json'
+YO_MAMA_JOKES_PATH = 'yo_mama_jokes.json'
 
-def main():
-
-    # Open joke source .json file
-    try:
-        with open(YO_MAMA_PATH) as json_file:
-            dict_jokes = json.load(json_file)
-    except:
-        print("Source file for jokes not found!")
-        exit()
+def printJoke():
 
     parser = ArgumentParser(prog='Yo Mama Jokes', description='Prints random "Yo Mama" jokes!', epilog='Need help?')
     parser.add_argument(nargs='*', dest="selection", type=str, help="Select joke types (keys in json)")
     args = parser.parse_args()
 
+          # Open joke source .json file
+    dict_jokes = JF.openFile(YO_MAMA_JOKES_PATH)
+
     # Find all jokes for selected types
-    selection = JokeFinder.checkJokeTypes(dict_jokes, args.selection)
+    selection = JF.checkJokeTypes(dict_jokes, args.selection)
 
     # Print random joke in terminal
-    print(JokeFinder.getRandomJoke(dict_jokes , selection))
+    print(JF.getRandomJoke(dict_jokes, selection))
+
+    myLabel.config(text = JF.getRandomJoke(dict_jokes, selection))
+
+# setup UI
+root = TK.Tk(screenName="Yo Mama Jokes", baseName="Yo Mama")
+root.geometry("400x400")
+root.title('"Yo Mama" Jokes Generator')
+myButton = TK.Button(root, text="Gimme a joke!",command=printJoke)
+myButton.pack()
+myLabel =TK.Label(root, text="")
+myLabel.pack()
+
+def main():
+
+# main loop for UI
+    root.mainloop()
 
 if __name__ == '__main__':
     main()
